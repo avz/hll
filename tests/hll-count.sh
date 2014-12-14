a96fd314ebb2a16cc71872ee8be1e957e0a8c60a
Artem Zaytsev <arepo@nologin.ru>
2014-12-14 13:53:29 +0400

    Результат теперь сравнивается с учётом погрешности и добавлена проверка хеша

--- tests/hll-count.sh
+++ tests/hll-count.sh
@@ -19,35 +19,55 @@ seq -s '-hll-count-test:' 0 9923 | tr : '\n' >> $TESTLOG
 
 ERRORS=0
 
+almost_equal() {
+	real=$1
+	expected=$2
+	percent=$3
+
+	min=$(($expected * (100 - $percent) / 100))
+	max=$(($expected * (100 + $percent) / 100))
+
+	test "$real" -ge "$min" -a "$real" -le "$max"
+}
+
 test_count() {
 	bits=$1
-	expected=$2
+	expected_count=$2
+	expected_hash=$3
 
-	count=$($SELFDIR/../bin/hll-count -b "$bits" < $TESTLOG)
+	result=$($SELFDIR/../bin/hll-count -d -b "$bits" < $TESTLOG)
 
-	if [ ! "$count" -eq "$expected" ]; then
-		echo "bits=$bits: $expected expected but $count given" 1>&2
+	hash=$(echo "$result" | cut -d' ' -f1)
+	count=$(echo "$result" | cut -d' ' -f2)
+
+	if ! almost_equal "$count" "$expected_count" 2; then
+		echo "bits=$bits: count $expected_count expected but $count given" 1>&2
+		ERRORS=$(($ERRORS + 1))
+	fi
+
+	if [ ! "$hash" -eq "$expected_hash" ]; then
+		echo "bits=$bits: hash $expected_hash expected but $hash given" 1>&2
 		ERRORS=$(($ERRORS + 1))
 	fi
 }
 
-test_count 4 1103249
-test_count 5 1412738
-test_count 6 1227010
-test_count 7 1124294
-test_count 8 1063738
-test_count 9 1020270
-test_count 10 1009955
-test_count 11 986528
-test_count 12 984061
-test_count 13 975120
-test_count 14 969761
-test_count 15 987246
-test_count 16 997976
-test_count 17 999960
-test_count 18 1001805
-test_count 19 1000120
-test_count 20 999507
+test_count 4 1103249 596423999
+test_count 5 1412738 1985856639
+test_count 6 1227010 133366178
+test_count 7 1124294 2975110038
+test_count 8 1063738 3756456334
+test_count 9 1020270 2227760783
+test_count 10 1009955 2097733064
+test_count 11 986528 2297798794
+test_count 12 984061 962787301
+test_count 13 975120 2698402778
+test_count 14 969761 4090902387
+test_count 15 987246 3745929533
+test_count 16 997976 879953779
+test_count 17 999960 160211267
+test_count 18 1001805 2936371152
+test_count 19 1000120 1575531669
+test_count 20 999507 3050768488
 
 if [ $ERRORS -gt 0 ]; then
 	echo "Errors count: $ERRORS" 1>&2
